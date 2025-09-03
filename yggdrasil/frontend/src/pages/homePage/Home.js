70a3723aa875e5575fe532c98a95e6e0dea42525
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2025-09-03 13:46:08 +0200

    more books and grid for the books

--- yggdrasil/frontend/src/pages/homePage/Home.js
+++ yggdrasil/frontend/src/pages/homePage/Home.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Home.css';
 import Navbar from './components/Navbar';
-import Book from './components/book';
+import BookCenterWrapper from './components/book';
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <div className="book-center-wrapper">
-        <Book />
-      </div>
+      <BookCenterWrapper />
     </div>
   );
 }
--- yggdrasil/frontend/src/pages/homePage/components/book.js
+++ yggdrasil/frontend/src/pages/homePage/components/book.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import coverImg from '../../../assets/images/cover.png';
-import campaignImg from '../../../assets/images/rose.jpg';
+import campaignImg1 from '../../../assets/images/rose.jpg';
+import campaignImg2 from '../../../assets/images/Logo.png';
+import campaignImg3 from '../../../assets/images/forest.jpg'; // Example third image
 import '../Home.css';
 
-const Book = ({ title = "Create a Campaign" }) => {
+const Book = ({
+  title = "Create a Campaign",
+  campaignImg,
+  blockColor = "#a65c2a",
+  lineColor = "#a65c2a"
+}) => {
   return (
     <div
       className="book-container"
@@ -12,14 +19,61 @@ const Book = ({ title = "Create a Campaign" }) => {
       }}
     >
       {/* Vertical line */}
-      <div className="book-vertical-line"></div>
+      <div
+        className="book-vertical-line"
+        style={{ background: lineColor }}
+      ></div>
       <div className="book-title">{title}</div>
       <div className="book-campaign-img-wrapper">
         <img className="book-campaign-img" src={campaignImg} alt="Campaign" />
       </div>
-      <div className="book-color-block"></div>
+      <div
+        className="book-color-block"
+        style={{ background: blockColor }}
+      ></div>
     </div>
   );
 };
 
-export default Book;
+const BookCenterWrapper = () => (
+  <div className="book-center-wrapper">
+    <Book
+      title="Campaign One"
+      campaignImg={campaignImg1}
+      blockColor="#a65c2a"
+      lineColor="#a65c2a"
+    />
+    <Book
+      title="Campaign Two"
+      campaignImg={campaignImg3}
+      blockColor="#199A6A"
+      lineColor="#199A6A"
+    />
+    <Book
+      title="Campaign Three"
+      campaignImg={campaignImg3}
+      blockColor="#3b2fa6"
+      lineColor="#3b2fa6"
+    />
+    <Book
+      title="Campaign One"
+      campaignImg={campaignImg1}
+      blockColor="#a65c2a"
+      lineColor="#a65c2a"
+    />
+    <Book
+      title="Campaign Two"
+      campaignImg={campaignImg3}
+      blockColor="#199A6A"
+      lineColor="#199A6A"
+    />
+    <Book
+      title="Campaign Three"
+      campaignImg={campaignImg3}
+      blockColor="#3b2fa6"
+      lineColor="#3b2fa6"
+    />
+  </div>
+);
+
+export default BookCenterWrapper;
